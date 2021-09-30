6ce5b2141f8cc986642419cf7ecbd5599a9b2ba3
Frederik <frederik.waeyaert@howest.Be>
2021-09-30 11:30:52 +0200

    Add project files.

---
+++ Demo/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Bier b1 = new Bier();
+            b1.naam = "Augustijn";
+            b1.brouwerij = "Bios";
+            //b1.alcoholpercentage = 12.0;
+            b1.SetAlcoholpercentage(12.0);
+            //b1.kleur = "Amber";
+            b1.Kleur = "Amber";
+
+            //Debug.WriteLine($"{b1.naam} - {b1.brouwerij} - {b1.alcoholpercentage}");
+            Debug.WriteLine($"{b1.naam} - {b1.brouwerij} - {b1.GetAlcoholpercentage()}");
+
+            Bier b2 = new Bier("Jupiler", "AB Inbev", 5.2, "blond");
+            Debug.WriteLine($"{b2.naam} - {b2.brouwerij} - {b2.GetAlcoholpercentage()}");
+
+            Bier b3 = new Bier("Omer", "Vander Ghinste", 8, "blond");
+            Debug.WriteLine($"{b3.naam} - {b3.brouwerij} - {b3.GetAlcoholpercentage()}");
+
+            Debug.WriteLine($"We hebben nu {Bier.bierCount} bier(en)");
+
+            //verschil berekenen tussen alcoholperc
+            //double alcoholverschil = b1.Alcoholverschil(b2);
+            //Debug.WriteLine($"Alcoholverschil tussen {b1.alcoholpercentage} en {b2.alcoholpercentage} = {alcoholverschil}");
+
+            //maak gebruik van een static method
+            double alcoholverschil = Bier.Alcoholverschil(b1, b2);
+            Debug.WriteLine($"Alcoholverschil tussen {b1.GetAlcoholpercentage()} en {b2.GetAlcoholpercentage()} = {alcoholverschil}");
+
+            Bier b4 = new Bier("Stella", "Inbev", -5.2, "Blond");
+            Debug.WriteLine(b4.ToString());
+
+            b4.SetAlcoholpercentage(-5);
+            Debug.WriteLine(b4.ToString());
+
+            Bier b5 = new Bier("Westmalle Tripel", "", 9, "");
+            Debug.WriteLine(b5.ToString());
+
+            b5.LocatieBrouwerij = "Westmalle";
+            Debug.WriteLine(b5.ExtendName);
+
+
+        }
+    }
+}
---
+++ Demo/Models/Bier.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Models
+{
+    public class Bier
+    {
+        public string naam;
+        public string brouwerij;
+        private double alcoholpercentage;
+        //public string kleur;
+
+        //propfull tab tab
+        private string kleur;
+
+        public string Kleur
+        {
+            get { return kleur; }
+            set {
+                if (value == "")
+                {
+                    kleur = "Blond";
+                }
+                else
+                {
+                    kleur = value;
+                }
+                
+            }
+        }
+
+
+
+        public static int bierCount;
+
+        public string LocatieBrouwerij { get; set; }
+
+        public string ExtendName
+        {
+            get { return this.naam + " - " + this.Kleur; }
+        }
+
+
+        //Voor aanmaak van een constructor kun je code-snippet ctor gebruiken
+        public Bier()
+        {
+            //naam = null;
+            //brouwerij = null;
+            //alcoholpercentage = 0.0;
+            //kleur = null;
+            bierCount++;
+        }
+
+        public Bier(string naam) : this()
+        {
+            this.naam = naam;
+            //bierCount++;
+        }
+
+        public Bier(string naam, string brouwerij, double alcoholperc, string kleur) : this()
+        {
+            this.naam = naam;
+            this.brouwerij = brouwerij;
+            this.alcoholpercentage = alcoholperc;
+            this.Kleur = kleur;
+            //bierCount++;
+        }
+
+        //public double Alcoholverschil(Bier someBeer)
+        //{
+        //    return Math.Abs(this.alcoholpercentage - someBeer.alcoholpercentage);
+        //}
+
+        public static double Alcoholverschil(Bier beer1, Bier beer2)
+        {
+            return Math.Abs(beer1.alcoholpercentage - beer2.alcoholpercentage);
+        }
+
+        public override string ToString()
+        {
+            return naam + " - " + brouwerij + " - " + alcoholpercentage + " - " + kleur; 
+        }
+
+
+        // dit gaan we vermijden, maak gebruik properties ivp methods
+        public void SetAlcoholpercentage(double alcoholpercentage)
+        {
+            if (alcoholpercentage < 0)
+            {
+                this.alcoholpercentage = 0;
+            }
+            else
+            {
+                this.alcoholpercentage = alcoholpercentage;
+            }
+        }
+
+        public double GetAlcoholpercentage() { return alcoholpercentage; }
+
+
+
+
+    }
+}
